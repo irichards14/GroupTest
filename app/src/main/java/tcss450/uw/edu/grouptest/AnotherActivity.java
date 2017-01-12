cc702e6f3f2116cf0c7315dc27461f8566f4f1c7
irichards14 <ian14@uw.edu>
2017-01-12 07:58:11 -0800

    Added dialog button with message

--- app/src/main/java/tcss450/uw/edu/grouptest/AnotherActivity.java
+++ app/src/main/java/tcss450/uw/edu/grouptest/AnotherActivity.java
@@ -10,4 +10,6 @@ public class AnotherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
     }
+
+
 }
--- app/src/main/java/tcss450/uw/edu/grouptest/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/grouptest/MainActivity.java
@@ -1,7 +1,11 @@
 package tcss450.uw.edu.grouptest;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDialog(View view) {
+        Context context = getApplicationContext();
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Bugs Bunny");
+        alertDialog.setMessage("What's up Doc?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Fudd Out",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
